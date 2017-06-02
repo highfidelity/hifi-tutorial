1adf461832d2e4b169dfd13094509cf4634d1652
Ryan Huffman <ryanhuffman@gmail.com>
2017-06-02 14:47:08 -0700

    Add cli to build.py

--- build.py
+++ build.py
@@ -1,28 +1,50 @@
 #!/usr/bin/env python
+"""
+This tool provides functionality to:
 
-#  assets/
-#      tutorial/...    // tutorial scripts, same path as it is now on assets server
-#       ...              // other assets
-#  entities/
-#      models.json
-#  domain-server/
-#      config.json
-#  build/
-#      assignment-client/
-#          assets/
-#              map.json
-#              files/
-#                  ...
-#          entities/
-#              models.json.gz
-#      domain-server/
-#          config.json
+ * Generate a content set directory that the High Fidelity domain server
+   and assignment clients can process.
+ * Package the generated content set into a releasable archive that the High
+   Fidelity Sandbox can download and use.
 
-import os
-import json
-import hashlib
+The tool expects the following directory structure:
+
+  assets/           # ATP server assets
+      ...
+  entities/         # Entity server assets, models.json in unzipped form
+      models.json
+  domain-server/    # Domain server assets
+      config.json
+
+Building the content set will process the above directory structure and produce
+a directory with a High Fidelity server compatible structures, which includes a
+gzipped models file and a map.json for the assets server.
+
+Build directory structure:
+
+  build/
+      assignment-client/
+          assets/
+              map.json
+              files/
+                  ...
+          entities/
+              models.json.gz
+      domain-server/
+          config.json
+
+Packaging the build will generate a gzipped tarball that can be downloaded and
+extracted by the Sandbox.
+
+"""
+
+import argparse
 import gzip
+import hashlib
+import json
+import os
 import shutil
+import sys
 import tarfile
 
 
@@ -31,15 +53,18 @@ def create_assets_map(file_path_pairs):
     for filename, path, filehash in file_path_pairs:
         path_parts = split(path)
         assets_path = '/' + '/'.join(path_parts)
+        if assets_path in assets_map:
+            print("**** Overwriting {}".format(assets_path))
         assets_map[assets_path] = filehash
     return assets_map
 
 
-def visit(root, dirs, files):
-    print('> ', root)
-
-
 def split(path):
+    """
+    Return a list containing the individual directories and filename (if
+    included) in `path`. This is in contract to os.path.split, which will only
+    split the path into 2 parts - the beginning and the last component.
+    """
     head, tail = os.path.split(path)
     if tail == '':
         if head == '':
@@ -50,6 +75,11 @@ def split(path):
 
 
 def makedirs(path):
+    """
+    Create directory `path`, including its parent directories if they do
+    not already exist. Return True if the directory did not exist and was
+    created, or False if it already existed.
+    """
     try:
         os.makedirs(path)
         return True
@@ -57,10 +87,12 @@ def makedirs(path):
         return False
 
 
-
 def generate_build(source_dir, output_dir, version=None):
-    print("# Generating release")
-
+    """
+    Generate a build by processing the directories and files in source_dir
+    and outputting the build to build_dir. if a version is specified, it will
+    be written to a file in the assignment-client directory.
+    """
     src_assets_dir = os.path.join(source_dir, 'assets')
     src_entities_dir = os.path.join(source_dir, 'entities')
     src_ds_dir = os.path.join(source_dir, 'domain-server')
@@ -76,11 +108,11 @@ def generate_build(source_dir, output_dir, version=None):
     makedirs(output_entities_dir)
     makedirs(output_ds_dir)
 
-    # Generate assets map
+    # Build asset server files
     print("\tWriting assets")
-    assets_files = []
     print("\t\tSource assets directory is: " + src_assets_dir)
     print("\t\tCopying assets to output directory")
+    assets_files = []
     for dirpath, dirs, files in os.walk(src_assets_dir):
         prefix = '  ' * len(split(os.path.relpath(dirpath, src_assets_dir)))
         #print(prefix + os.path.relpath(dirpath, src_assets_dir) + '\\')
@@ -106,7 +138,6 @@ def generate_build(source_dir, output_dir, version=None):
     print("\tWriting entities")
     models_filepath = os.path.join(src_entities_dir, 'models.json')
     output_models_filepath = os.path.join(output_entities_dir, 'models.json.gz')
-
     if os.path.exists(output_models_filepath):
         print("\t\tmodels.json.gz in build directory already exists, "
               + "not overwriting")
@@ -123,26 +154,29 @@ def generate_build(source_dir, output_dir, version=None):
 
     shutil.copy(src_ds_config_filepath, output_ds_config_filepath)
 
+    # Write content version
     print("\tWriting content version")
-    output_version_filepath = os.path.join(output_ac_dir, 'content-version.txt')
     if version is None:
         print("\t\tSkipping, version not specified")
     else:
         print("\t\tWriting version {}".format(version))
+        output_version_filepath = os.path.join(output_ac_dir, 'content-version.txt')
         with open(output_version_filepath, 'w') as f:
             f.write(str(version))
 
     print("\tComplete")
 
 
-RELEASE_PATHS = (
+PATHS_TO_INCLUDE_IN_ARCHIVE = (
         'assignment-client/assets/files',
         'assignment-client/assets/map.json',
         'assignment-client/entities/models.json.gz',
         'assignment-client/content-version.txt',
         'domain-server/config.json',
         )
-def generate_release(input_dir, output_filepath):
+
+
+def generate_package(input_dir, output_filepath):
     print("# Generating release")
 
     if not output_filepath.endswith('.tar.gz'):
@@ -155,7 +189,7 @@ def generate_release(input_dir, output_filepath):
 
         print("\tWriting archive to {}".format(output_filepath))
         with tarfile.open(output_filepath, 'w:gz') as f:
-            for path in RELEASE_PATHS:
+            for path in PATHS_TO_INCLUDE_IN_ARCHIVE:
                 full_path = os.path.join(input_dir, path)
                 print("\t\tAdding to archive: {}".format(full_path))
                 f.add(full_path, path, filter=tarfilter)
@@ -163,10 +197,34 @@ def generate_release(input_dir, output_filepath):
     print("\tComplete")
 
 
-if __name__ == '__main__':
-    root_dir = os.getcwd()
-    source_dir = root_dir
-    output_dir = os.path.join(root_dir, 'build')
+def handle_generate_build(args):
+    output_dir = args.output_directory
+    source_dir = os.getcwd()
+
+    print("Generating build in `{}` from `{}`".format(output_dir, source_dir))
+
     generate_build(source_dir, output_dir, 35)
+
+
+def handle_generate_package(args):
     archive_path = os.path.join(root_dir, 'home-content-{}.tar.gz'.format(35))
-    generate_release(output_dir, archive_path)
+    generate_package(output_dir, archive_path)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Content set generator and packager.')
+    subparsers = parser.add_subparsers()
+
+    parser_gen_build = subparsers.add_parser('gen_build', help='generate build')
+    parser_gen_build.set_defaults(func=handle_generate_build)
+    parser_gen_build.add_argument('output_directory', default='build', nargs='?')
+
+    parser_package = subparsers.add_parser('package', help='generate release package')
+    parser_package.set_defaults(func=handle_generate_package)
+    parser_package.add_argument('output_filename')
+
+    args = parser.parse_args(sys.argv[1:])
+    if 'func' in args:
+        args.func(args) 
+    else:
+        parser.print_help()
